4ca3c122c35c71842be8918515a087ad32e01aaf
pyeseo <seopw28@gmail.com>
2025-04-08 11:54:59 +0900

    ratio

---
+++ 7_Kaggle_Df/Pokemon/poke.py
@@ -0,0 +1,17 @@
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+df = pd.read_csv('pokemon_df.csv')
+df.head()
+
+# %%
+
+# %%
+
+# %%
+
+# %%
