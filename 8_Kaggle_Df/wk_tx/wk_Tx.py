e72182ad0c374f1b78baad7f5b9e570750fbe465
pyeseo <seopw28@gmail.com>
2025-04-11 10:22:39 +0900

    pivot

---
+++ 8_Kaggle_Df/wk_tx/wk_Tx.py
@@ -0,0 +1,14 @@
+#%% 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns   
+
+# %%
+# Load the data
+df = pd.read_csv('wk_tx.csv')
+df.head()
+
+# %%
+
+
