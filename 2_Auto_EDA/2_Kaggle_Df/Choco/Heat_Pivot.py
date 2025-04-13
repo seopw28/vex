386f71d4a28c01f1c7adf8e21ec0fa3c9c15be37
pyeseo <seopw28@gmail.com>
2025-04-13 23:44:54 +0900

    eda

--- 2_Auto_EDA/2_Kaggle_Df/Choco/Heat_Pivot.py
+++ 2_Auto_EDA/2_Kaggle_Df/Choco/Heat_Pivot.py
@@ -6,8 +6,6 @@ df = pd.read_csv("chocolate.csv")
 df.head()
 
 # %%
-
-
 # Create pivot table of salesperson vs country with sum of boxes shipped
 pivot_table = pd.pivot_table(df, 
                            values='Boxes Shipped',
@@ -33,7 +31,7 @@ plt.title('Boxes Shipped by Sales Person and Country')
 plt.tight_layout()
 
 # Save the plot as an image file
-plt.savefig('sales_heatmap.png', dpi=300, bbox_inches='tight')
+plt.savefig('heat_map_sales.png', dpi=300, bbox_inches='tight')
 plt.show()
 
 # %%
